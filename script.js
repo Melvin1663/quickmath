13f72eebd40c59498105f9da6b7ec9bc8fd88d3c
Melvin1663 <johnmelvinlabad800@gmail.com>
2023-02-09 09:48:15 +0800

    restart function and auto detect function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputElement = document.getElementById('input');
 const problemElement = document.getElementById('problem');
 const timeLeftElement = document.getElementById('timeLeft');
+const restartElement = document.getElementById('restart');
 
 let counter = 0;
 let endTime;
@@ -30,33 +31,56 @@ const randomMathQuestion = (max) => {
     problemElement.innerHTML = res;
 }
 
+const restart = () => {
+    randomMathQuestion();
+
+    restartElement.style.setProperty('visibility', 'hidden');
+
+    inputElement.disabled = false;
+    inputElement.value = '';
+
+    counter = 0;
+
+    timeLeftElement.innerHTML = 'Answer to start';
+}
+
 randomMathQuestion();
 
 const input = document.querySelector("input");
 
-input.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        if (!inputElement.value || inputElement.value != eval(problemElement.innerHTML.replace('×', '*').replace('÷', '/'))) return;
+const correctAnswer = () => {
+    // if (!inputElement.value || inputElement.value != eval(problemElement.innerHTML.replace('×', '*').replace('÷', '/'))) return;
 
-        if (!counter) {
-            endTime = Date.now() + 30000;
+    if (!counter) {
+        endTime = Date.now() + 30000;
 
-            let interval = setInterval(() => {
-                if ((endTime - Date.now()) <= 0) return timeLeftElement.innerHTML = `You scored ${counter-1}!`;
-                timeLeftElement.innerHTML = `<code>${((endTime - Date.now()) / 1000).toFixed(1)}s</code> Left`
-            }, 100)
+        let interval = setInterval(() => {
+            if ((endTime - Date.now()) <= 0) return timeLeftElement.innerHTML = `You scored ${counter - 1}!`;
+            timeLeftElement.innerHTML = `<code>${((endTime - Date.now()) / 1000).toFixed(1)}s</code> Left`
+        }, 100)
 
-            setTimeout(() => {
-                inputElement.disabled = true;
-                inputElement.value = '';
+        setTimeout(() => {
+            inputElement.disabled = true;
+            inputElement.value = '';
 
-                clearInterval(interval);
-            }, 30000)
-        }
+            restartElement.style.setProperty('visibility', 'visible')
 
-        counter++;
-
-        inputElement.value = '';
-        randomMathQuestion();
+            clearInterval(interval);
+        }, 30000)
     }
+
+    counter++;
+
+    inputElement.value = '';
+    randomMathQuestion();
+}
+
+input.addEventListener("keydown", (event) => {
+
+    if (event.key === "Enter" && inputElement.value == eval(problemElement.innerHTML.replace('×', '*').replace('÷', '/'))) correctAnswer();
 });
+
+input.addEventListener("keyup", (event) => {
+    // console.log(`${inputElement.value}${event.key}` == eval(problemElement.innerHTML.replace('×', '*').replace('÷', '/')))
+    if (inputElement.value == eval(problemElement.innerHTML.replace('×', '*').replace('÷', '/'))) correctAnswer();
+})
